60aa7b93b3a2d18e3aeae5dcb9183732365d92ae
AlejandraRamirez <ma.ramirez@uniandes.edu.co>
2018-04-10 18:08:07 -0500

    3

--- AlejandraRamirez_S11C1.cpp
+++ AlejandraRamirez_S11C1.cpp
@@ -2,39 +2,45 @@
 
 using namespace std;
 
-double func_prime(double x, double y);
+double func_primeS(double I, double S, double B);
+double func_primeI(double I, double S, double B, double g);
 
 int main () {
    //Variables del problema
    int Nt = 771;
    int I0 = 1;
    int s0 = 770;
+   //caso 1
    double B1 = 0.0022;
    double g1 = 0.45;
+   //caso 2
    double B2 = 0.001;
    double g2 = 0.2;
 
    //Variables leap-frog
    double h = 0.01;
    double min_x = 0.0;
-   double max_x = 4.0;
+   double max_x = Nt;
    int N = int((max_x-min_x)/h);
 
    //Arrays de ceros
-   double xx[N];
-   double yy[N];
+   double ss[N];
+   double II[N];
    
    for (int i = 0; i < N; i++) {
-       xx[i] = 0;
-       yy[i] = 0;
+       ss[i] = 0;
+       II[i] = 0;
    }
 
-   xx[0] = min_x;
-   yy[0] = 1.0;
+   II[0] = I0;
+   ss[0] = s0;
+
+   II[1] = II[0] + h*func_primeI(II[0],ss[0],B1,g1);
+   ss[1] = ss[0] + h*func_primeS(II[0],ss[0],B1);
 
    for (int i = 2; i < N; i++) {
-       xx[i] = xx[i-1] + h;
-       yy[i] = yy[i-2] + 2*h*func_prime(xx[i-1],yy[i-1]);
+       II[i] = II[i-2] + 2*h*func_primeI(II[i-1],ss[i-1],B1,g1);
+       ss[i] = ss[i-2] + 2*h*func_primeS(II[i-1],ss[i-1],B1);
    }
 
 
@@ -42,7 +48,12 @@ int main () {
    return 0;
 }
 
-double func_prime(double x, double y){
-   double sol = -y;
+double func_primeS(double I, double S, double B){
+   double sol = -B*I*S;
+   return sol;
+}
+
+double func_primeI(double I, double S, double B, double g){
+   double sol = B*I*S - g*I;
    return sol;
 }
